33e91c5a06ce355600b6de654e4e9d14d3b52d2a
Gonzalo Flores <gonzalo.flores@lana.xyz>
2021-03-14 00:44:26 -0300

    add model contribution

--- models/contribution.go
+++ models/contribution.go
@@ -2,7 +2,6 @@ package models
 
 // Contribution structure that contains the information of a contribution by candidate
 type Contribution struct {
-	ID          string `json:"id"`
 	Contributor string `json:"contributor"`
 	Candidate   string `json:"candidate"`
 	URI         string `json:"uri"`
